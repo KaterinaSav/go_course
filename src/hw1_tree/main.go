afecf15eb2e89cdaca864790fe9105996380ff84
KaterinaSav <katsiaryna.sauchuk@itechart-group.com>
2020-02-27 19:21:57 +0300

    Show directories.

--- src/hw1_tree/main.go
+++ src/hw1_tree/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 	"path/filepath"
 	"strings"
 )
+
+const defaultCounter = 3
 
 func main() {
 	out := os.Stdout
@@ -21,19 +24,87 @@ func main() {
 	}
 }
 
-type errorStruct struct {
-    Message string
-}
 
-func (err errorStruct) Error() string {
-    if err.Message != "" {
-        return err.Message
+func dirTree(out io.Writer, path string, printFiles bool) error {
+    level := 0
+    err := visitAllDir(out, path, printFiles, level)
+    if err != nil {
+        return err
     }
-    return "Something bad happened"
+    return nil
 }
 
-func dirTree(out *os.File, path string, printFiles bool) *errorStruct {
-    err := errorStruct{Message: "test"}
-    return &err
+func visitAllDir(out io.Writer, path string, printFiles bool, level int) error {
+    var files []os.FileInfo
+    files, err := visitDir(path, printFiles)
+    if err != nil {
+        return err
+    }
+    sort.Slice(files, func(i, j int) bool { return files[i].Name() < files[j].Name() })
+    filesLength := len(files)
+    for index, file := range files {
+        if (file.IsDir()) {
+            lastFileIndex := filesLength - 1 == index
+            filename := filepath.Join(path, "/", file.Name())
+            if index == 0 {
+                level++
+            }
+            printFile(file.Name(), level, lastFileIndex)
+            err := visitAllDir(out, filename, printFiles, level)
+            if err != nil {
+                return err
+            }
+        }
+    }
+    return nil
 }
 
+func printFile(name string, level int, lastFileIndex bool) {
+    levels := make([]int, level)
+    var lastResult string
+    var firstResult string
+    for index, _ := range levels {
+        lastIndex := index + 1 == level
+        firstIndex := index == 0 && !lastIndex
+        beforeLastIndex := index + 2 == level && !firstIndex
+        if beforeLastIndex {
+            firstResult = firstResult + strings.Repeat(" ", defaultCounter)
+        }
+
+        if lastIndex {
+            if lastFileIndex {
+                lastResult = "└"
+            } else {
+                lastResult = "├"
+            }
+            lastResult = lastResult + strings.Repeat("─", defaultCounter)
+        }
+        if !beforeLastIndex && !lastIndex {
+            firstResult = firstResult + " │" + strings.Repeat(" ", defaultCounter)
+        }
+    }
+
+    fmt.Println(firstResult, lastResult, name, level)
+}
+
+func visitDir(path string, printFiles bool) (files []os.FileInfo, err error) {
+    f, err := os.Open(path)
+    if err != nil {
+        return files, err
+    }
+    files, err = f.Readdir(-1)
+    f.Close()
+    if err != nil {
+        return files, err
+    }
+    if printFiles {
+        return files, err
+    }
+    var onlyDirFiles []os.FileInfo
+    for _, file := range files {
+        if (file.IsDir()) {
+            onlyDirFiles = append(onlyDirFiles, file)
+        }
+    }
+    return onlyDirFiles, err
+}
