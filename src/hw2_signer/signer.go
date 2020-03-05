a50007edd59157815bb0ea1277486976d6398197
KaterinaSav <katsiaryna.sauchuk@itechart-group.com>
2020-03-05 17:16:42 +0300

    Add goroutines and channels exercise.

--- src/hw2_signer/signer.go
+++ src/hw2_signer/signer.go
@@ -2,50 +2,101 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"sync"
+	"runtime"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("Started")
+	inputData := []int{0,1}
+
+    hashSignJobs := []job{
+        job(func(in, out chan interface{}) {
+            for _, fibNum := range inputData {
+                fmt.Println("save to out")
+                fmt.Println(fibNum)
+                out <- fibNum
+            }
+        }),
+        job(SingleHash),
+        job(MultiHash),
+        job(func(in, out chan interface{}) {
+            dataRaw := <-in
+            data := fmt.Sprintf("%v", dataRaw)
+            fmt.Println("data result" + data)
+        }),
+    }
+
+    ExecutePipeline(hashSignJobs...)
+    fmt.Scanln()
 }
 
 func ExecutePipeline(jobs ...job) {
-	ch1 := make(chan interface {}, 1)
-	ch2 := make(chan interface {}, 1)
+	in := make(chan interface {}, 3)
+    out := make(chan interface {}, 3)
+    wg := &sync.WaitGroup{}
 	for _, job := range jobs {
-	    go job(ch1, ch2)
+        wg.Add(1)
+	    go Pipeline(wg, job, in, out)
 	}
+
+	go func(in, out chan interface {}) {
+        for {
+            select {
+                case <-out:
+                    fmt.Println("save out to in")
+                    data := fmt.Sprintf("%v", <-out)
+                    fmt.Printf(data)
+                    in <- data
+                case <-in:
+                    data := fmt.Sprintf("%v", <-in)
+                                fmt.Printf(data)
+                 fmt.Println("something saved to in")
+            }
+        }
+    }(in, out)
+	wg.Wait()
+}
+
+func Pipeline(wg *sync.WaitGroup, currentJob job, in chan interface {}, out chan interface {}) {
+    defer wg.Done()
+    currentJob(in, out)
+    runtime.Gosched()
 }
 
 var SingleHash = func(in, out chan interface{}) {
     dataRaw := <-in
-    var (
-    	string1 string
-    	string2 string
-    )
-    data := dataRaw.(string)
-    string1 = DataSignerCrc32(data)
-    string2 = DataSignerCrc32(DataSignerMd5(data))
-    out <- string1 + "~" + string2
+    data := fmt.Sprintf("%v", dataRaw)
+    fmt.Println(data + " SingleHash " + "data " + data)
+    md5Data := DataSignerMd5(data)
+    fmt.Println(data + " SingleHash " + "md5(data) " + md5Data)
+    crc32md5Data := DataSignerCrc32(md5Data)
+    fmt.Println(data + " SingleHash " + "crc32(md5(data)) " + crc32md5Data)
+    crc32Data := DataSignerCrc32(data)
+    fmt.Println(data + " SingleHash " + "crc32(data) " + crc32Data)
+    result := crc32Data + "~" + crc32md5Data
+    fmt.Println(data + " SingleHash " + "result " + result)
+    out <- result
 }
 
 var MultiHash = func(in, out chan interface{}) {
     dataRaw := <-in
-    data := dataRaw.(string)
-    iterationsNum := 5
-    hashResults := make([]string, 0, iterationsNum + 1)
+    data := fmt.Sprintf("%v", dataRaw)
+    iterationsNum := 6
+    result := ""
     for i := 0; i < iterationsNum; i++ {
-        th = i.(string)
-        crcResult = DataSignerCrc32(th + data)
-        hashResults = append(hashResults, crcResult)
+        th := strconv.Itoa(i)
+        crcResult := DataSignerCrc32(th + data)
+        fmt.Println(data + " MultiHash " + "crc32(th+step1)) " + th + " " + crcResult)
+        result = result + crcResult
     }
-    out <- hashResults
+    fmt.Println(data + " MultiHash " + "result " + result)
+    out <- result
 }
 
 var CombineResults = func(in, out chan interface{}) {
     dataRaw := <-in
-    sort.Strings(dataRaw)
-    var dataString string
-    dataString := strings.Join(dataRaw, "_")
-    out <- dataString
+    data := dataRaw.(string)
+    out <- data
 }
