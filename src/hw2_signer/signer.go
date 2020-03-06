97d42fb6b41374ada72208a65d28a87fb3f93f80
KaterinaSav <katsiaryna.sauchuk@itechart-group.com>
2020-03-06 21:38:08 +0300

    go and channels

--- src/hw2_signer/signer.go
+++ src/hw2_signer/signer.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"runtime"
 	"strconv"
 )
 
@@ -33,66 +31,65 @@ func main() {
 }
 
 func ExecutePipeline(jobs ...job) {
-	in := make(chan interface {}, 3)
-    out := make(chan interface {}, 3)
-    wg := &sync.WaitGroup{}
-	for _, job := range jobs {
-        wg.Add(1)
-	    go Pipeline(wg, job, in, out)
-	}
-
-	go func(in, out chan interface {}) {
-        for {
-            select {
-                case <-out:
-                    fmt.Println("save out to in")
-                    data := fmt.Sprintf("%v", <-out)
-                    fmt.Printf(data)
-                    in <- data
-                case <-in:
-                    data := fmt.Sprintf("%v", <-in)
-                                fmt.Printf(data)
-                 fmt.Println("something saved to in")
-            }
-        }
-    }(in, out)
-	wg.Wait()
+    in := make(chan interface {})
+    out := make(chan interface {})
+	Pipeline(jobs, in , out)
 }
 
-func Pipeline(wg *sync.WaitGroup, currentJob job, in chan interface {}, out chan interface {}) {
-    defer wg.Done()
-    currentJob(in, out)
-    runtime.Gosched()
+func Pipeline(jobs []job, in chan interface {}, out chan interface {}) {
+    middleCh1 := make(chan interface {}, 5)
+    middleCh2 := make(chan interface {}, 5)
+
+    go jobs[0](in, middleCh1)
+    go jobs[1](middleCh1, middleCh2)
+    go jobs[2](middleCh2, out)
 }
 
 var SingleHash = func(in, out chan interface{}) {
-    dataRaw := <-in
-    data := fmt.Sprintf("%v", dataRaw)
-    fmt.Println(data + " SingleHash " + "data " + data)
-    md5Data := DataSignerMd5(data)
-    fmt.Println(data + " SingleHash " + "md5(data) " + md5Data)
-    crc32md5Data := DataSignerCrc32(md5Data)
-    fmt.Println(data + " SingleHash " + "crc32(md5(data)) " + crc32md5Data)
-    crc32Data := DataSignerCrc32(data)
-    fmt.Println(data + " SingleHash " + "crc32(data) " + crc32Data)
-    result := crc32Data + "~" + crc32md5Data
-    fmt.Println(data + " SingleHash " + "result " + result)
-    out <- result
+    for {
+        select {
+            case dataRaw, ok := <-in:
+            if !ok {
+                close(out)
+                return
+            }
+               data := fmt.Sprintf("%v", dataRaw)
+               fmt.Println(data + " SingleHash " + "data " + data)
+               md5Data := DataSignerMd5(data)
+               fmt.Println(data + " SingleHash " + "md5(data) " + md5Data)
+               crc32md5Data := DataSignerCrc32(md5Data)
+               fmt.Println(data + " SingleHash " + "crc32(md5(data)) " + crc32md5Data)
+               crc32Data := DataSignerCrc32(data)
+               fmt.Println(data + " SingleHash " + "crc32(data) " + crc32Data)
+               result := crc32Data + "~" + crc32md5Data
+               fmt.Println(data + " SingleHash " + "result " + result)
+               out <- result
+            }
+        }
+
 }
 
 var MultiHash = func(in, out chan interface{}) {
-    dataRaw := <-in
-    data := fmt.Sprintf("%v", dataRaw)
-    iterationsNum := 6
-    result := ""
-    for i := 0; i < iterationsNum; i++ {
-        th := strconv.Itoa(i)
-        crcResult := DataSignerCrc32(th + data)
-        fmt.Println(data + " MultiHash " + "crc32(th+step1)) " + th + " " + crcResult)
-        result = result + crcResult
-    }
-    fmt.Println(data + " MultiHash " + "result " + result)
-    out <- result
+    for {
+        select {
+            case dataRaw, ok := <-in:
+                if ok == false {
+                    close(out)
+                    return
+                }
+                data := fmt.Sprintf("%v", dataRaw)
+                iterationsNum := 6
+                result := ""
+                for i := 0; i < iterationsNum; i++ {
+                    th := strconv.Itoa(i)
+                    crcResult := DataSignerCrc32(th + data)
+                    fmt.Println(data + " MultiHash " + "crc32(th+step1)) " + th + " " + crcResult)
+                    result = result + crcResult
+                }
+                fmt.Println(data + " MultiHash " + "result " + result)
+                out <- result
+            }
+        }
 }
 
 var CombineResults = func(in, out chan interface{}) {
